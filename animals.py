ab63ba6a133b3a05e45c6841b7b86f0b774d4329
Nemari <baksfyl@gmail.com>
2020-06-05 02:10:31 +0300

    +game coregameplay +animals refactoring again

--- animals.py
+++ animals.py
@@ -1,40 +1,50 @@
-from abc import ABCMeta, abstractmethod, abstractproperty
 import random
 import pygame
-import game_config as gc
-class Product():
 
-    def __init__(self, name, price, dim):
+
+class GameObject():
+    def __init__(self, picture):
+        self.picture = picture
+
+class Product(GameObject):
+
+    def __init__(self, name, price, dim, picture):
+        super().__init__(self)
         self.name = name
         self.price = price
         self.amount = 0
         self.dim = dim
+        self.picture = picture
+
 
 class Milk(Product):
     def __init__(self):
-        super().__init__(name = "Milk", price = 7, dim = "litres")
+        super().__init__(name = "Milk", price = 7, dim = "litres", picture = "assets\milk.png")
 
 
 
 class Egg(Product):
     def __init__(self):
-        super().__init__(name="Egg", price=5, dim = "")
+        super().__init__(name="Egg", price=5, dim = "", picture = "assets\egg.png")
 
 
 class Wool(Product):
     def __init__(self):
-        super().__init__(name="Wool", price=10, dim = "kilos")
+        super().__init__(name="Wool", price=10, dim = "kilos", picture = "assets\wool.png")
 
 
-class Animal():
-    def __init__(self, name, action, product, price):
+class Animal(GameObject):
+    def __init__(self, name="", action="", product="", price=0, picture=""):
+        super().__init__(self)
         self.name = name
         self.action = action        #action you can do with animal
         self.price = price
         self.state = 'set'
         self.ripen = pygame.USEREVENT + 1
         self.hunger = 0
-        self.product = Product(product.name, product.price, product.dim)
+      #  self.product = Product(product.name, product.price, product.dim)
+        self.picture = picture
+        self.product = product
     def info(self):
         print("{} is an animal. It is in the amount of {}. It gives you {}.You can {} it.".format(self.name, self.amount, self.product, self.action))
     def event1(self):
@@ -75,14 +85,14 @@ class Action():
 
 class Cow(Animal):
     def __init__(self):
-        super().__init__(name="Cow", action="milk", price=10, product = Milk())
+        super().__init__(name="Cow", action="milk", price=10, product = Milk(), picture = "assets\cow.png")
 
 class Chicken(Animal):
     def __init__(self):
-        super().__init__(name="Chicken", action="pick_eggs", price=3, product =Egg())
+        super().__init__(name="Chicken", action="pick_eggs", price=3, product =Egg(), picture = "assets\chicken.png")
 
 class Sheep(Animal):
     def __init__(self):
-        super().__init__(name="Sheep", action="shear", price=5, product = Wool())
+        super().__init__(name="Sheep", action="shear", price=5, product = Wool(), picture = "assets\sheep.png" )
 
 
--- coregameplay.py
+++ coregameplay.py
@@ -1,6 +1,11 @@
 import pygame
 import time
 import random
+from pygame import display, event, image, transform
+import game_config as gc
+import threading
+from animals import Cow, Sheep, Animal, Action,  GameObject
+from player import maria
 
 pygame.init()
 
@@ -86,7 +91,136 @@ def game_intro():
 
 
 def game_loop():
-   pass
+    class FarmField():
+        def __init__(self, num):
+            self.farm_tiles = {}
+            self.size = str(num) + 'x' + str(num)
+            '''for i in range(0, num**2):
+                self.farm_tiles[i]=None'''
+
+    farm_test = FarmField(3)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            farm_test.farm_tiles[i, j] = None
+
+    def find_index(x, y):
+        row = y // gc.IMAGE_SIZE
+        col = x // gc.IMAGE_SIZE
+        index = row * gc.NUM_TILES_SIDE + col
+        return index
+
+    def pic_load(gameobject):
+        picture = pygame.image.load(gameobject.picture)
+        picture = pygame.transform.scale(picture, (gc.IMAGE_SIZE - gc.MARGIN, gc.IMAGE_SIZE - gc.MARGIN))
+        return picture
+
+    def find_row(y):
+        row = y // gc.IMAGE_SIZE
+        return row
+
+    def find_col(x):
+        col = x // gc.IMAGE_SIZE
+        return col
+
+    class Clocks:
+        def init(self, days, previous_time, hours):
+            self.hours = hours
+            self.days = days
+            self.previous_time = previous_time
+
+    pygame.init()
+    display.set_caption('fam farm')
+    screen = display.set_mode((384, 384))
+    tiles = [*farm_test.farm_tiles]
+    print(tiles)
+
+    def change_pic(pic):
+        new_pic = image.load(pic)
+        new_pic = transform.scale(new_pic, (gc.IMAGE_SIZE - gc.MARGIN, gc.IMAGE_SIZE - gc.MARGIN))
+        screen.blit(new_pic, (col * gc.IMAGE_SIZE, row * gc.IMAGE_SIZE))
+
+    def stall(farm_obj, farm, x, y):  # set an animal to a place
+        farm.farm_tiles[(x, y)] = farm_obj
+
+    def choose(farm, x, y):
+        return farm.farm_tiles[(x, y)]
+
+    def draw_go(player, obj):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        index = find_index(mouse_x, mouse_y)
+        row = find_row(mouse_y)
+        col = find_col(mouse_x)
+        player.stall(obj, farm_test, col, row)
+        screen.blit(pic_load(obj), (col * gc.IMAGE_SIZE, row * gc.IMAGE_SIZE))
+
+    class Shop():
+        def __init__(self, player):
+            self.player = player
+
+        def buy_sheep(self):
+            sheep = Sheep()
+            return sheep
+
+        def buy_cow(self):
+            cow = Cow()
+            return cow
+
+        def shopping(self, event):
+            if event.key == pygame.K_w:
+                return shop.buy_cow()
+            elif event.key == pygame.K_a:
+                return shop.buy_sheep()
+
+    act = Action()
+    animal = Animal()
+    shop = Shop(maria)
+    running = True
+    grass = pygame.image.load('assets\grass.jpg')
+    # cow_pic=pygame.image.load('assets\cow.png')
+    # cow_pic=pygame.transform.scale(cow_pic, (gc.IMAGE_SIZE-gc.MARGIN, gc.IMAGE_SIZE-gc.MARGIN))
+    grass = transform.scale(grass, (gc.IMAGE_SIZE - gc.MARGIN, gc.IMAGE_SIZE - gc.MARGIN))
+    for i in tiles:
+        screen.blit(grass, (i[0] * gc.IMAGE_SIZE, i[1] * gc.IMAGE_SIZE))
+    counter = 10
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # Usually wise to be able to close your program.
+                raise SystemExit
+            if event.type == pygame.KEYDOWN:
+                animal = shop.shopping(event)
+            animal_l = Animal()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                index = find_index(mouse_x, mouse_y)
+                row = find_row(mouse_y)
+                col = find_col(mouse_x)
+                if animal is not None:
+                    maria.stall(animal, farm_test, col, row)
+                    screen.blit(pic_load(animal), (col * gc.IMAGE_SIZE, row * gc.IMAGE_SIZE))
+
+                animal_l = choose(farm_test, row, col)
+
+            if animal_l is not None:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_s:
+                        act.feed(animal_l)
+                    elif event.key == pygame.K_d:
+                        act.get_product(animal_l)
+
+                # elif animal is not None:
+                #     maria.stall(animal, farm_test, col, row)
+                #     screen.blit(pic_load(animal), (col * gc.IMAGE_SIZE, row * gc.IMAGE_SIZE))
+                #     continue
+                # else:
+                #     continue
+
+                # if animal.state == 'sleep':
+                #     timer = threading.Timer(5.0, change_pic, ['assets\cow_sleep.png'])
+                #     timer.start()
+
+        display.flip()
 
 def gamequit():
     pygame.quit()
